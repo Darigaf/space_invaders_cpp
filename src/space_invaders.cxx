0c68bcf44d99387c2b90cb7e0bdb3f6484efc4c2
Mateus <mateus@mail.mateusbernardo.xyz>
2024-05-03 15:14:58 -0300

    started doing the main screen

--- src/space_invaders.cxx
+++ src/space_invaders.cxx
@@ -1,25 +1,34 @@
 #include <ncurses.h>
-void iniciar_tela(){
+void start_screen(){
+	int max_rows, max_cols;
+	getmaxyx(stdscr, max_rows, max_cols);
+	//start screen with some defined atributes
 	initscr();
 	raw();	
 	noecho();
 	keypad(stdscr, TRUE);
+	//start default color scheme
 	start_color();
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
 	attron(COLOR_PAIR(1));
+
 }
-void encerrar_tela(){
+void end_screen(){
 	endwin();
 	attroff(COLOR_PAIR(1));
 }
+int main_screen(){
+	char game_title[]="SPACE INVADERS";
+	char game_title_pt_br[]="OS INVASORES DO ESPAÇO";
+	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+	
+
+}
+
 int main(){
-	char nome_jogador;
-	iniciar_tela();
-	printw("SPACE INVADERS");
-	nome_jogador = getch();
-	printw("%c", nome_jogador);
+	start_screen();
 	refresh();
 	getch();
-	encerrar_tela();	
+	end_screen();	
 	return 0;
 }
